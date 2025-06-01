6ceb5f22ff1a4305e52b90e87a94723f8a5ec71d
vpilane <vpilane@gmail.com>
2025-06-02 00:22:37 +0200

    first commi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar.tsx';
 import About from './pages/About.tsx';
